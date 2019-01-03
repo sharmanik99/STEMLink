cfa9e2195d2954684e8264197f93b44d0641e742
Nikhil Sharma <sharmanikhil99@gmail.com>
2019-01-03 12:44:07 -0500

    STEMLink Updated

---
+++ app/src/main/java/com/example/nikhil/stemlinkupdated/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.nikhil.stemlinkupdated;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void Loginclick(View view){
+        Intent intent = new Intent(MainActivity.this, Navigation.class);
+        startActivity(intent);
+
+
+    }
+}
